de99b4d62afd446ec9d9c27ccad2510a6dfefc95
Jose <jose.zuniga1145@gmail.com>
2025-02-03 20:27:43 -0600

    1.0.05

--- src/app/lib/db.js
+++ src/app/lib/db.js
@@ -1,17 +1,15 @@
 import mysql from "mysql2/promise";
 
+// Crear un pool de conexiones usando la variable DATABASE_URL
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  uri: process.env.DATABASE_URL, // Usa DATABASE_URL directamente
   waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-  idleTimeout: 60000, // Opcional: cerrar conexiones inactivas después de 60s
+  connectionLimit: 10, // Máximo número de conexiones simultáneas
+  queueLimit: 0, // Sin límite de solicitudes en cola
+  idleTimeout: 60000, // Cerrar conexiones inactivas después de 60s (opcional)
 });
 
-// Opción 1: Usar el pool directamente
+// Opción 1: Usar el pool directamente para ejecutar consultas
 export async function query(sql, params) {
   const [rows] = await pool.execute(sql, params);
   return rows;
