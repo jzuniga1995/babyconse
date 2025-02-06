501e343fde664d8c71f87cce90ad2031541d832c
Jose <jose.zuniga1145@gmail.com>
2025-02-06 10:36:13 -0600

    1.0.06

--- src/app/sitemap.xml/route.js
+++ src/app/sitemap.xml/route.js
@@ -1,31 +1,15 @@
 export async function GET() {
-  const staticUrls = [
-    { url: "https://www.saludyser.com", lastModified: new Date().toISOString() },
-    { url: "https://www.saludyser.com/contacto", lastModified: new Date().toISOString() },
-    { url: "https://www.saludyser.com/foro", lastModified: new Date().toISOString() },
-    { url: "https://www.saludyser.com/politica", lastModified: new Date().toISOString() },
-    { url: "https://www.saludyser.com/terminos", lastModified: new Date().toISOString() },
-    { url: "https://www.saludyser.com/nosotros", lastModified: new Date().toISOString() },
-  ];
-
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ${staticUrls
-      .map(
-        ({ url, lastModified }) => `
-      <url>
-        <loc>${url}</loc>
-        <lastmod>${lastModified}</lastmod>
-      </url>
-    `
-      )
-      .join("")}
+    <url>
+      <loc>https://www.saludyser.com</loc>
+      <lastmod>${new Date().toISOString()}</lastmod>
+    </url>
   </urlset>`;
 
   return new Response(sitemap, {
     headers: {
       "Content-Type": "application/xml",
-      "Cache-Control": "no-store, no-cache, must-revalidate, proxy-revalidate",
     },
   });
 }
