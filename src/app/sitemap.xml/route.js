755800df27d41bb22a50ad79d51d5a60c4a6d974
Jose <jose.zuniga1145@gmail.com>
2025-02-06 10:45:07 -0600

    1.0.06

--- src/app/sitemap.xml/route.js
+++ src/app/sitemap.xml/route.js
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic"; // Fuerza renderización en cada solicitud
+
 export async function GET() {
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
@@ -10,6 +12,7 @@ export async function GET() {
   return new Response(sitemap, {
     headers: {
       "Content-Type": "application/xml",
+      "Cache-Control": "no-store, no-cache, must-revalidate, proxy-revalidate", // Evita el caché
     },
   });
 }
