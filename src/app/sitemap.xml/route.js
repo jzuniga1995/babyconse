18be98ccf028adea66c9bea418b756ef64997b06
jzuniga1995 <113640443+jzuniga1995@users.noreply.github.com>
2025-02-10 15:17:06 +0000

    imagenes/favicon

--- src/app/sitemap.xml/route.js
+++ src/app/sitemap.xml/route.js
@@ -27,7 +27,7 @@ export async function GET() {
   try {
     const connection = await getConnection();
     const articlesQuery = `
-      SELECT slug, published_at 
+      SELECT slug, published_at, image, title
       FROM articulos
       ORDER BY published_at DESC
     `;
@@ -48,10 +48,14 @@ export async function GET() {
     console.error("Error al cargar categorías dinámicas:", error.message);
   }
 
-  // Generar URLs dinámicas para artículos
+  // Generar URLs dinámicas para artículos (incluyendo imágenes)
   const articleUrls = articles.map((article) => ({
     url: `https://www.saludyser.com/articulos/${encodeURIComponent(article.slug)}`,
     lastModified: new Date(article.published_at || new Date()).toISOString(),
+    image: {
+      url: `https://www.saludyser.com/images/articulos/${article.image}`,
+      title: article.title || "",
+    },
   }));
 
   // Generar URLs dinámicas para categorías
@@ -65,16 +69,24 @@ export async function GET() {
 
   // Generar el XML del sitemap
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
     ${urls
-      .map(
-        ({ url, lastModified }) => `
+      .map(({ url, lastModified, image }) => `
       <url>
         <loc>${url}</loc>
         <lastmod>${lastModified}</lastmod>
+        ${
+          image
+            ? `
+        <image:image>
+          <image:loc>${image.url}</image:loc>
+          <image:title>${image.title}</image:title>
+        </image:image>
+        `
+            : ""
+        }
       </url>
-    `
-      )
+    `)
       .join("")}
   </urlset>`;
 
